3a57ad226ec751745d2c5ccd8b36fc155e4547b0
AMOPBOT <134328176+AMOPBOT@users.noreply.github.com>
2024-01-10 22:52:36 +0530

    Update kora.py

--- kora.py
+++ kora.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("rm -rf misspoppy && git clone https://github.com/AbhiMod/misspoppy && cd misspoppy && python3 -m Yone")
+system ("rm -rf misspoppy && git clone https://github.com/AMOPBOT/misspoppy && cd misspoppy && python3 -m Yone")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
